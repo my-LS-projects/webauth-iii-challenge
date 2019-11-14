52ce7faa76ea1f817eaa27077887a3a7715bdaf2
frescocodes <hueydakotah@gmail.com>
2019-11-13 22:22:42 -0500

    MVP: endpoints done, jwt auth working

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const server = express();
+const usersRouter = require('../database/users/users-router')
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', usersRouter)
 
 server.get('/', ( req, res ) => {
     res.status(200).json({ message: "api running" })
